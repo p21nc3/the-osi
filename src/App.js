5687e01008a43ff0349c4cc48ba213bd29fdb5d9
p21nc3 <pp4768@srmist.edu.in>
2023-08-16 17:27:22 +0530

    try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import RepositoryAnalyzer from "./views/RepositoryAnalyzer";
+import UserDashboard from "./views/UserDashboard";
+import Register from "./components/Register";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./views/Login";
+import Hero from "./views/Hero";
+import CriticalValues from "./views/CriticalValues";
 
 function App() {
+  // const [loggedIn, setLoggedIn] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path="/repo-analyser" element={<RepositoryAnalyzer />} />
+      <Route path="/user-analyser" element={<UserDashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/critical-values" element={<CriticalValues />} />
+      <Route path="/" element={<Hero />} />
+      </Routes> 
+    </>
   );
 }
 
 export default App;
+
+
+/**
+ * Fix Login Page
+ * Change props
+ * Landing page abhi 
+ */
